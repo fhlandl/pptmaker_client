c797ee4b2401e7a736ede7e70b2687a01cc57426
fhlandl <mkj1206@gmail.com>
2023-02-19 22:30:14 +0900

    Todo 추가
    - Todo를 변경하여 앱을 만들기 위함

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import { useState } from 'react';
+import TodoList from './components/TodoList/TodoList';
+import Header from './components/Header/Header';
+
+const filters = ['all', 'active', 'completed'];
+function App() {
+  const [filter, setFilter] = useState(filters[0]);
+  return (
+    <>
+      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
+    </>
+  );
+}
+
+export default App;
