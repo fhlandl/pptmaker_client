06c41bf5b7e8b9f27a3e8bfda6d856642e5bb85f
fhlandl <mkj1206@gmail.com>
2023-02-21 23:43:03 +0900

    안쓰는 import 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css';
 import { useState } from 'react';
 import TodoList from './components/TodoList/TodoList';
-import Header from './components/Header/Header';
 import NavigationBar from './components/NavigationBar/NavigationBar';
 
 const menus = ['말씀', '찬양', '기도', '소식', 'PPT'];
