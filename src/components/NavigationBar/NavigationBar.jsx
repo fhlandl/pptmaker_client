40fddd95a4e77ddd238173aafe5d85719dab3d61
fhlandl <mkj1206@gmail.com>
2023-02-26 19:06:48 +0900

    메뉴 레이아웃 핸드폰 화면 고려

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { FaChurch } from 'react-icons/fa';
+import { RxDropdownMenu } from 'react-icons/rx';
 import styles from './NavigationBar.module.css';
 
 export default function NavigationBar({ menus, menu, onMenuChange }) {
+  const listRef = useRef();
+  const toggleBtn = () => {
+    listRef.current.classList.toggle(styles.active);
+  };
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
         <FaChurch className={styles.icon} />
         <div className={styles.text}>창후교회</div>
       </div>
-      <ul className={styles.menu}>
+      <ul className={styles.menu} ref={listRef}>
         {menus.map((value, index) => (
           <li key={index} className={styles.listitem}>
             <button
@@ -23,6 +28,9 @@ export default function NavigationBar({ menus, menu, onMenuChange }) {
           </li>
         ))}
       </ul>
+      <button className={styles.toggleBtn} onClick={toggleBtn}>
+        <RxDropdownMenu className={styles.icon} />
+      </button>
     </nav>
   );
 }
